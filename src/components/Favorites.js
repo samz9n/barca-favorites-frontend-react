87abec03f2cf4171d87b38ef1daaf3d8298a579e
samz9n <samuel.langhoff@outlook.com>
2022-11-11 14:37:23 +0200

    added function for adding and removing favorites. Also added localstorage function

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,7 +1,69 @@
-import React from 'react'
+import { Card, CardContent, Box, Grid, Paper, Button, Typography } from '@mui/material';
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import Players from './Players';
 
-export const Favorites = () => {
-  return (
-    <div>Favorites</div>
-  )
-}
+export const Favorites = (props) => {
+	//REMOVES SELECTED PLAYER FROM FAVORITES
+	const removeFavorite = (id) => {
+		let updatedFavorites = [ ...props.favorites ].filter((i) => i.playerId !== id);
+		props.setFavorites(updatedFavorites);
+	};
+
+	return (
+		<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+			<Button sx={{ margin: '1rem' }} variant="contained" component={Link} to="/players">
+				Back to adding favorites
+			</Button>
+			{/* IF THERE IS NO ADDED FAVORITES, SHOW TEXT */}
+			{props.favorites.length === 0 && (
+				<Typography color="white" variant="h6">
+					No added favorites!
+				</Typography>
+			)}
+			<Grid sx={{ flexGrow: 1 }} container spacing={2}>
+				<Grid item xs={12}>
+					<Grid container justifyContent="center" spacing={2}>
+						{/* MAP OVER ALL FAVORITE PLAYERS */}
+						{props.favorites.map((player) => {
+							return (
+								<Grid key={player.playerId} item>
+									<Paper
+										sx={{
+											display: 'flex',
+											flexDirection: 'column',
+											height: 250,
+											width: 200,
+											alignItems: 'center',
+											justifyContent: 'space-between'
+										}}
+									>
+										<Typography variant="h6">
+											{player.firstName} {player.lastName}
+										</Typography>
+										<Typography>{player.position}</Typography>
+										<Typography>{'Age: ' + player.age}</Typography>
+										<Typography>{'Number: ' + player.shirtNumber}</Typography>
+										<Button
+											sx={{ width: '100%' }}
+											variant="contained"
+											color="error"
+											onClick={(e) => {
+												e.preventDefault();
+												localStorage.removeItem('Player' + player.playerId);
+												removeFavorite(player.playerId);
+											}}
+										>
+											Remove
+										</Button>
+									</Paper>
+								</Grid>
+							);
+						})}
+					</Grid>
+				</Grid>
+			</Grid>
+			<Outlet />
+		</Box>
+	);
+};
